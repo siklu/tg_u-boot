46f1d223e34ec22c9e596a005695b34dff0e5779
Shmuel H <sh@tkos.co.il>
2020-05-27 09:55:32 +0300

    fixup

--- drivers/led/led_mdio.c
+++ drivers/led/led_mdio.c
@@ -73,7 +73,7 @@ struct udevice* led_mdio_create (const char* label, struct phy_device *phydev, i
 {
 	struct led_mdio_priv *priv;
 	struct udevice* dev;
-	device_bind_driver(phydev->dev, "mdio_led", label, &dev);
+	device_bind_driver(gd->dm_root, "mdio_led", label, &dev);
 	if (dev == NULL) {
 		printf("ERROR: could not create mdio_led driver");
 		return NULL;

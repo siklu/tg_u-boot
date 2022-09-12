712c8cb772faf24b401ab74f42c5b8cd27f16c05
Paul T <55404485+paul-sirin@users.noreply.github.com>
2022-09-12 11:28:45 +0300

    feature/DEV-4969-add-counter-for-failed-boots (#22)
    This PR adds support for boot counter to support failover switch to another software bank. It uses device_mgmt and works in following way:
     *  Default value for boot_tries_left > 0 is set by supporting software, decrease it.
     * When boot_tries_left = 0 -- switch bank.
     * When boot_tries_left < 0 -- ignore it (no supporting software has booted after switch).

--- common/siklu/bank_management.h
+++ common/siklu/bank_management.h
@@ -1,14 +1,23 @@
 #ifndef __SIKLU_BANK_MANAGEMENT_H__
 #define __SIKLU_BANK_MANAGEMENT_H__
 
+#include <linux/types.h>
+
 struct software_bank_t {
 	const char* bank_label;
 };
 
 /**
- * Get the current software bank.
+ * Get the current software bank, update current bank if failover happened
+ * @param do_failover  do a bank switch and save changes
  * @return a pointer to a valid software_bank_t.
  */
-struct software_bank_t* bank_management_get_current_bank(void);
+struct software_bank_t* bank_management_get_current_bank(bool do_failover);
+
+/**
+ * Get current boot_tries_left, decrease value for next boot
+ * @return count of boot tries left or -1 on fail
+ */
+int8_t bank_management_get_boot_tries_left();
 
 #endif

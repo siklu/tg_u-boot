c8e5fcc956308c936beb7ced4000644a0b5aae8d
Dmitry Grinberg <dima.g@siklu.com>
2023-03-10 02:08:14 +0200

    wip

--- cmd/siklu/siklu_bubt.c
+++ cmd/siklu/siklu_bubt.c
@@ -1,5 +1,10 @@
+/*
+ * Copyright 2023 by Siklu Ltd. All rights reserved.
+ */
+
 #include <common.h>
 #include <version.h>
+#include <string.h>
 
 #if 0
 
@@ -60,6 +65,7 @@ static int do_siklu_bubt(cmd_tbl_t *cmdtp, int flag, int argc, char *const argv[
 {
 	const char *expected_siklu_ver;
 	const char *file_name;
+	const char *actual_siklu_ver;
 
 	if (argc != 3) {
 		return CMD_RET_USAGE;
@@ -70,6 +76,18 @@ static int do_siklu_bubt(cmd_tbl_t *cmdtp, int flag, int argc, char *const argv[
 	printf("expected_siklu_ver=%s\n", expected_siklu_ver);
 	printf("file_name=%s\n", file_name);
 	printf("version_string=%s\n", version_string);
+
+	actual_siklu_ver = strrchr(version_string, ' ');
+	if (!actual_siklu_ver) {
+		printf("Invalid u-boot version string [%s]\n", version_string);
+		return CMD_RET_FAILURE;
+	}
+	actual_siklu_ver++;
+	if (*actual_siklu_ver == '\0') {
+		printf("Invalid actual u-boot version string\n");
+		return CMD_RET_FAILURE;
+	}
+	printf("actual_siklu_ver=[%s]\n", actual_siklu_ver);
 
 	return CMD_RET_SUCCESS;
 #if 0
@@ -84,11 +102,12 @@ U_BOOT_CMD(
 		3,
 		0,
 		do_siklu_bubt,
-		"siklu_bubt <expected-siklu-version> <file-name>",
+		"<expected-siklu-version> <file-name>\n",
 		"If the actual u-boot version matches the expected\n"
-		"siklu version, this command will do nothing.\n"
-		"Overwise, load the image from tftp and burn a u-boot\n"
-		"image to flash and then reboot the device.\n"
+		"siklu version of u-boot, this command will do nothing.\n"
+		"Overwise, this command will load the u-boot image from"
+		"tftp server and burn the u-boot image to the NOR flash\n"
+		"and then reboot the device.\n"
 		"Example:\n"
 		"\tsiklu_bubt 1.0.0-21894-2b1d294a35 flash-image.bin\n"
 );
